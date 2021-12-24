9756c49151dc9d60306280b7e269542a25f43d95
mwg <mwg@whut.edu.com>
2021-12-24 10:42:43 +0800

    update

--- myTelnetClient.py
+++ myTelnetClient.py
@@ -55,6 +55,7 @@ class TelnetClient:
             self.input(cmd)
         res = self.get_output()
         # res 裁剪首尾行（首行：输入的命令；尾行：输入提示符）
+        res = '\n'.join(res.split('\n')[1:-1])
         print("===================")
         print(res)
         print("===================")
